d820e88cafd854d5d6d5ed7c9a483f123a955cd3
Sahilgupta89177 <sahilgupta89177@gmail.com>
2024-06-02 17:38:56 +0530

    commit3

--- 1.Lets Start/controller/product.js
+++ 1.Lets Start/controller/product.js
@@ -1,15 +1,29 @@
-const getData=async (req,res)=>{
-   res.status(200).json(" I am in realdata")
+const product_schema = require("../models/product")
+
+const getData = async (req, res) => {
+    //1
+    //     const user =await product_schema.find({});
+    //    res.status(200).json({user});
+
+    //2   using request object
+    //     const data=req.body;
+    //     console.log({"name":data.name})
+    //     const user =await product_schema.find({"username":data.name});
+    //    res.status(200).json({user});
+
+    //3 using query param
+    const user = await product_schema.find(req.query);
+    res.status(200).json({ user });
 }
 
-const getTestData =async (req,res)=>{
+const getTestData = async (req, res) => {
     res.status(200).json("i am in getTestData")
 }
 
-const getpostdata =async (req,res)=>{
+const getpostdata = async (req, res) => {
     const data = req.body; // assuming you're sending JSON data in the request body
     // Logic to handle the posted data
- 
-    res.status(200).json({ message: "Data received",data:req.body  });
+
+    res.status(200).json({ message: "Data received", data: req.body });
 }
-module.exports={getData,getTestData,getpostdata}
+module.exports = { getData, getTestData, getpostdata }
--- 1.Lets Start/db/connect.js
+++ 1.Lets Start/db/connect.js
@@ -1,9 +1,9 @@
-// URL ="mongodb+srv://sahilgupta:WoeY3wqEQ2R9na6Q@dumpdata.jkmz7iw.mongodb.net/"
+URL ="mongodb+srv://sahilgupta:WoeY3wqEQ2R9na6Q@dumpdata.jkmz7iw.mongodb.net/"
 
 const mongoose =require("mongoose")
 
 
-const connnectDb =(URL)=>{
+const connnectDb =()=>{
 
     try {
          mongoose.connect(URL);
